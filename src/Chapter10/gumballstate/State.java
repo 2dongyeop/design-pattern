aefe20cd85791c6cb2d5d69448c7f739c5d93d0c
leedongyeop <ldy_1204@naver.com>
2022-08-16 14:45:08 +0900

    상태별로 클래스를 만들어 책임을 위임

---
+++ src/Chapter10/gumballstate/State.java
@@ -0,0 +1,12 @@
+package Chapter10.gumballstate;
+
+public interface State {
+
+    public void insertQuarter();
+    public void ejectQuarter();
+    public void turnCrank();
+    public void dispense();
+
+    public void refill();
+}
+
