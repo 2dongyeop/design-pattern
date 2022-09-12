0737d820b4acc34f4914d4c397405e1612007722
leedongyeop <ldy_1204@naver.com>
2022-09-12 15:12:22 +0900

    원격 서비스 인터페이스와 구현 클래스

---
+++ src/Chapter11/proxy/rmi/MyRemoteImpl.java
@@ -0,0 +1,25 @@
+package Chapter11.proxy.rmi;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
+    private static final long serialVersionUID = 1L;
+
+    public String sayHello() {
+        return "Server says, 'Hey'";
+    }
+
+    protected MyRemoteImpl() throws RemoteException {
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyRemote service = new MyRemoteImpl();
+            Naming.rebind("RemoteHello", service);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
