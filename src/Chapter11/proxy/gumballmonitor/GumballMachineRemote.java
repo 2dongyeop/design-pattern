d8a7d3e7a048a6b1828cd550fa0fb7f31a82b262
leedongyeop <ldy_1204@naver.com>
2022-09-13 15:15:21 +0900

    직렬화 준비

---
+++ src/Chapter11/proxy/gumballmonitor/GumballMachineRemote.java
@@ -0,0 +1,10 @@
+package Chapter11.proxy.gumballmonitor;
+
+import java.rmi.*;
+
+public interface GumballMachineRemote extends Remote {
+    public int getCount() throws RemoteException;
+    public String getLocation() throws RemoteException;
+    public State getState() throws RemoteException;
+}
+
