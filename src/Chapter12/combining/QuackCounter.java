dc328285c65a8b44b1c8e6118554e757e244d4e8
leedongyeop <ldy_1204@naver.com>
2022-09-22 14:51:34 +0900

    duck decorator

---
+++ src/Chapter12/combining/QuackCounter.java
@@ -0,0 +1,20 @@
+package Chapter12.combining;
+
+public class QuackCounter implements Quackable {
+    Quackable duck;
+    static int numberOfQuacks;
+
+    public QuackCounter(Quackable duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public void quack() {
+        duck.quack();
+        numberOfQuacks++;
+    }
+
+    public static int getQuacks() {
+        return numberOfQuacks;
+    }
+}
