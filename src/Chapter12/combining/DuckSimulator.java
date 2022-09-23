5bf0597edbe2d4dda7aa19de90bcee612b53a7e6
leedongyeop <ldy_1204@naver.com>
2022-09-23 13:53:43 +0900

    시뮬레이터에 컴포지트 패턴과 반복자 패턴을 적용

--- src/Chapter12/combining/DuckSimulator.java
+++ src/Chapter12/combining/DuckSimulator.java
@@ -15,15 +15,36 @@ public class DuckSimulator {
         Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\n오리 시뮬레이션 게임");
+        System.out.println("\n오리 시뮬레이션 게임 : 무리 (+컴포지트)");
 
-        simulate(mallardDuck);
-        simulate(redheadDuck);
-        simulate(duckCall);
-        simulate(rubberDuck);
-        simulate(gooseDuck);
+        Flock flockOfDucks = new Flock();
 
-        System.out.println("오리가 소리 낸 횟수 : " + QuackCounter.getQuacks() + " 번");
+        flockOfDucks.add(redheadDuck);
+        flockOfDucks.add(duckCall);
+        flockOfDucks.add(rubberDuck);
+        flockOfDucks.add(gooseDuck);
+
+        Flock flockOfMallards = new Flock();
+
+        Quackable mallardOne = duckFactory.createMallardDuck();
+        Quackable mallardTwo = duckFactory.createMallardDuck();
+        Quackable mallardThree = duckFactory.createMallardDuck();
+        Quackable mallardFour = duckFactory.createMallardDuck();
+
+        flockOfMallards.add(mallardOne);
+        flockOfMallards.add(mallardTwo);
+        flockOfMallards.add(mallardThree);
+        flockOfMallards.add(mallardFour);
+
+        flockOfDucks.add(flockOfMallards);
+
+        System.out.println("\n오리 시뮬레이션 게임 : 전체 무리");
+        simulate(flockOfDucks);
+
+        System.out.println("\n오리 시뮬레이션 게임 : 물오리 무리");
+        simulate(flockOfMallards);
+
+        System.out.println("\n오리가 소리 낸 횟수 : " + QuackCounter.getQuacks() + " 번");
     }
 
     private void simulate(Quackable duck) {
