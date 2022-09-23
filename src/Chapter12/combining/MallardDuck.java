81747b8a24450d171de0e08e0e325c242024734a
leedongyeop <ldy_1204@naver.com>
2022-09-23 14:32:44 +0900

    Observer 보조 객체와 Quackable 클래스 결합

--- src/Chapter12/combining/MallardDuck.java
+++ src/Chapter12/combining/MallardDuck.java
@@ -1,8 +1,26 @@
 package Chapter12.combining;
 
+import java.util.Observer;
+
 public class MallardDuck implements Quackable {
+    Observable observable;
+
+    public MallardDuck() {
+        observable = new Observable(this);
+    }
+
     @Override
     public void quack() {
         System.out.println("꽥꽥");
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
     }
 }
