ffb4783e4daa4298d92ab422da5f1bfcf0bd213f
leedongyeop <ldy_1204@naver.com>
2022-09-24 15:02:40 +0900

    심박수 test code

---
+++ src/Chapter12/mvc/HeartTestDrive.java
@@ -0,0 +1,10 @@
+package Chapter12.mvc;
+
+public class HeartTestDrive {
+
+    public static void main (String[] args) {
+        HeartModel heartModel = new HeartModel();
+        ControllerInterface model = new HeartController(heartModel);
+    }
+}
+
